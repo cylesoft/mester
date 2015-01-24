f6ea3e8d7fd308f5b69bc45d47847b615f5ad615
Cyle Gage <cylegage@gmail.com>
2015-01-24 15:58:07 -0500

    now uses digital sigs to verify sender, though need to put in infrastructure for distributed user info

--- mester.js
+++ mester.js
@@ -9,6 +9,7 @@
 var readline = require('readline');
 var dgram = require('dgram');
 var fs = require('fs');
+var crypto = require('crypto');
 var uuid = require('node-uuid');
 var keypair = require('keypair');
 
@@ -16,6 +17,7 @@ var rl = readline.createInterface(process.stdin, process.stdout);
 var socket = dgram.createSocket('udp4');
 var identity_filename = 'identity.mester';
 
+var users = {};
 var this_username = 'nobody';
 var this_uuid = '000';
 var this_keypair = {};
@@ -49,7 +51,7 @@ function identity_step() {
 			this_username = identity.username;
 			this_uuid = identity.uuid;
 			this_keypair.public = identity.public_key;
-			this.keypair.private = identity.private_key;
+			this_keypair.private = identity.private_key;
 			console.log('Loaded identity `'+this_username+'` ('+this_uuid+') with public/private keypair.');
 			connection_step();
 		}
@@ -62,8 +64,20 @@ function connection_step() {
 		var incoming_message = JSON.parse(data.toString());
 		//console.log(incoming_tweet);
 		if (incoming_message.t == 'chat') {
-			console.log(''+incoming_message.un+' ('+incoming_message.uuid+'): '+incoming_message.msg);
+			// verify incoming message with signature
+			// get the incoming UUID's public key
+			var verifier = crypto.createVerify('RSA-SHA256');
+			verifier.update(incoming_message.msg);
+			var verified = verifier.verify(users[incoming_message.uuid].public_key, incoming_message.sig, 'base64');
+			if (verified) {
+				console.log(''+users[incoming_message.uuid].username+' ('+incoming_message.uuid+'): '+incoming_message.msg);
+			} else {
+				console.log('incoming message from "'+incoming_message.un+'" not verified!');
+			}
 		} else if (incoming_message.t == 'presence') {
+			if (users[incoming_message.uuid] == undefined) {
+				users[incoming_message.uuid] = { "username": incoming_message.un, "public_key": incoming_message.public_key };
+			}
 			console.log(''+incoming_message.un+' ('+incoming_message.uuid+') is now online!');
 		} else if (incoming_message.t == 'query') {
 			// send back stuff
@@ -83,7 +97,7 @@ function connection_step() {
 function chat_step() {
 	
 	// we're here!
-	var first_message = JSON.stringify({"t":"presence","un":this_username,"uuid":this_uuid});
+	var first_message = JSON.stringify({"t":"presence","un":this_username,"uuid":this_uuid,"public_key":this_keypair.public});
 	socket.send(new Buffer(first_message), 0, first_message.length, mester_port, broadcast_address, function (err) {
 		if (err) { console.log(err); }
 	});
@@ -110,8 +124,11 @@ function chat_step() {
 			break;
 			default:
 			// send out your message in proper serialized form
-			var new_message = JSON.stringify({"t":"chat","un":this_username,"uuid":this_uuid,"msg":new_message});
-			socket.send(new Buffer(new_message), 0, new_message.length, mester_port, broadcast_address, function (err) {
+			var signer = crypto.createSign('RSA-SHA256');
+			signer.update(new_message);
+			var message_signature = signer.sign(this_keypair.private, 'base64');
+			var message_object_serialized = JSON.stringify({ "t": "chat", "uuid": this_uuid, "msg": new_message, "sig": message_signature });
+			socket.send(new Buffer(message_object_serialized), 0, message_object_serialized.length, mester_port, broadcast_address, function (err) {
 				if (err) { console.log(err); }
 				console.log("Message sent!");
 				rl.prompt();
